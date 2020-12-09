ebf2172d8e1e7eb3878193ee73dfeae0fc9dda7d
Luiz Reina <luizreeina@gmail.com>
2020-12-09 19:40:04 -0300

    projeto weather forecast web - v 0.0.1

---
+++ weather.js
@@ -0,0 +1,72 @@
+const submitCity = document.getElementById('submit-city');
+const divCityName = document.getElementById('name-id');
+const divCityTemperature = document.getElementById('temperature-id');
+const otherInfos = document.getElementById('infos');
+const nextDaysSection = document.getElementById('next-days-id');
+
+const createNextDaysDivs = () => {
+  for (let index = 0; index <= 9; index += 1) {
+    const nextDayParagraph = document.createElement('p');
+    const nextDayDiv = document.createElement('div');
+    nextDayDiv.className = "next-day-class";
+    nextDayParagraph.id = index;
+    nextDayDiv.appendChild(nextDayParagraph);
+    nextDaysSection.appendChild(nextDayDiv);    
+  }
+}
+
+createNextDaysDivs();
+
+const appendResults = (data) => {
+  const { city, date, time, description, temp, wind_speedy, humidity } = data.results;
+
+  divCityName.innerText = `${city}
+  ${date}, ${time}
+  ${description}`;
+
+  divCityTemperature.innerText = `${temp}°C
+  ${(temp * 1.8 + 32).toPrecision(2)}°F`;
+
+  otherInfos.innerText = `Vento: ${wind_speedy}
+  Humidade: ${humidity}%`;
+
+  console.log(data.results);
+}
+
+const appendForecast = (data) => {
+  let counter = 0;
+  data.results.forecast.forEach((element) => {
+    const p = document.getElementById(`${counter}`);
+    p.innerText = `${element.weekday}, ${element.date}
+    ${element.min}° à ${element.max}°
+    ${element.description}`;
+    counter += 1;
+  });
+}
+
+const fetchWheater = (cityCode) => {
+  fetch(`https://api.hgbrasil.com/weather?key=2207bfed&format=json-cors&city_name=${cityCode}`)
+  .then((response) => {
+    response.json().then((data) => {
+      appendResults(data);
+      appendForecast(data);
+    })
+  })
+}
+
+const fetchWheaterByIp = () => {
+  fetch(`https://api.hgbrasil.com/weather?key=2207bfed&format=json-cors&user_ip=remote`)
+  .then((response) => {
+    response.json().then((data) => {
+      appendResults(data);
+      appendForecast(data);
+    })
+  })
+}
+
+submitCity.addEventListener('click', (event) => {
+  const input = document.getElementById('choose-city');
+  fetchWheater(input.value);
+});
+
+window.onload = fetchWheaterByIp();
